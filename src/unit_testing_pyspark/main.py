f5b40b6b6b22ed8f379486d5d30e2b42595906b3
luijkr <luijk.r@gmail.com>
2025-02-14 11:47:13 +0100

    rewrite using DABs

---
+++ src/unit_testing_pyspark/main.py
@@ -0,0 +1,11 @@
+"""Main functions for the unit_testing_pyspark package."""
+
+
+def main() -> bool:
+    """Entry point for the unit_testing_pyspark package."""
+    print("Hello, RevoData! \nWheel task is successfully completed.")
+    return True
+
+
+if __name__ == "__main__":  # pragma: no cover
+    main()
